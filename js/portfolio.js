c7ad47d75e6d6afa03d14e447379ca33c7dadfc9
Lazar <kojiclazar89@gmail.com>
2024-10-07 12:34:22 -0700

    upload files

---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+// Function to open modal
+function openModal(modalId) {
+    document.getElementById('modal' + modalId).style.display = 'flex';
+}
+
+// Function to close modal
+function closeModal(modalId) {
+    document.getElementById('modal' + modalId).style.display = 'none';
+}
